17df9d8ab343a5e9001ee596d4da96a61366c993
Zulqarnain Majeed <zulqarnainjutt16@gmail.com>
2021-02-28 01:45:30 +0200

    Changed email display to display name

--- src/components/fragments/Navigation_bar.vue
+++ src/components/fragments/Navigation_bar.vue
@@ -29,7 +29,7 @@ export default {
     created() {
         if (firebase.auth().currentUser) {
             (this.isLoggedIn = true),
-                (this.currentUser = firebase.auth().currentUser.email);
+                (this.currentUser = firebase.auth().currentUser.displayName);
         }
     },
     components: {
